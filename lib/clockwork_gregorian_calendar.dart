acb940c0de7e72ed9cf333ecf35a6687cc24507c
Yi Hong <hongyiweiwu@outlook.com>
2020-01-06 19:17:47 +0800

    Reorganized units export.

--- lib/clockwork_gregorian_calendar.dart
+++ lib/clockwork_gregorian_calendar.dart
@@ -3,8 +3,4 @@ library clockwork_gregorian_calendar;
 export 'src/calendar.dart';
 export 'src/timestamp_extension.dart';
 
-export 'src/units/era.dart';
-export 'src/units/length.dart';
-export 'src/units/month.dart';
-export 'src/units/quarter.dart';
-export 'src/units/weekday.dart';
+export 'src/units/units.dart';
